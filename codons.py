866b7e3203e98b632a7e926b690352a1df9e5984
BenBenaroya <dbb161199@gmail.com>
2024-12-17 12:49:54 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,12 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  file = open(file_path)
+  rows = file.readlines()
+  file.close()
+
+  mapping_c_a = {}
+  for row in rows:
+    cell = row.strip().split('\t')
+    mapping_c_a[cell[0]] = cell[2]
+  return mapping_c_a
 
 
